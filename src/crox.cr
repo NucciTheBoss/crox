40c416d37be6247318bd70365d7387ead8e5c878
NucciTheBoss <nucci.programming@gmail.com>
2022-06-27 23:52:47 -0400

    Base structure of interpreter launcher

--- src/crox.cr
+++ src/crox.cr
@@ -1,6 +1,30 @@
-# TODO: Write documentation for `Crox`
-module Crox
-  VERSION = "0.1.0"
+# Copyright 2022 Jason C. Nucciarone
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-  # TODO: Put your code here
+VERSION = "0.1.0"
+
+class Crox
+  def Crox.main
+    if ARGV.size > 1
+      STDERR.puts("USAGE: crox [script]")
+    elsif ARGV.size == 1
+      puts(ARGV[0])
+    else
+      # TODO: Define REPL code
+      puts "# TODO"
+    end
+  end    
 end
+
+Crox.main
